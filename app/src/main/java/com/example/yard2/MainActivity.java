346b43f0e1a5d4388000bd17da578c521f7f3060
Diablo <diabloexodia@gmail.com>
2023-06-17 16:53:24 +0530

    new change

--- app/src/main/java/com/example/yard2/MainActivity.java
+++ app/src/main/java/com/example/yard2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.yard2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println(1);
         System.out.println();
+        // the following change was made by @diabloexodia at 4:51 pm 17th-June-2023
+
+        Toast.makeText(this, "New test change", Toast.LENGTH_SHORT).show();
     }
 }
