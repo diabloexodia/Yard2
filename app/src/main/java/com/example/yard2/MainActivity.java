c5f298a4c134cf5b60eb88a77e545d1b950daae9
Pranith12250 <127399614+Pranith12250@users.noreply.github.com>
2023-06-17 21:26:24 +0530

    ok

--- app/src/main/java/com/example/yard2/MainActivity.java
+++ app/src/main/java/com/example/yard2/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println(1);
         System.out.println();
+        System.out.println();
     }
 }
