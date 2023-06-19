dde1d695e166aba789539d258b280a6324ceddef
Pranith12250 <127399614+Pranith12250@users.noreply.github.com>
2023-06-19 10:31:45 +0530

    all major activities created

--- app/src/main/java/com/example/yard2/MainActivity.java
+++ app/src/main/java/com/example/yard2/MainActivity.java
@@ -2,15 +2,52 @@ package com.example.yard2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+    Button qrbutton, productreceivingbutton, productdispatchbutton, statusbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        qrbutton=findViewById(R.id.qrbutton);
+        productreceivingbutton=findViewById(R.id.productreceivingbutton);
+        productdispatchbutton=findViewById(R.id.productdispatchbutton);
+
+        qrbutton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent qrpage=new Intent(MainActivity.this, QRGenerator.class);
+                startActivity(qrpage);
+            }
+        });
+
+        productreceivingbutton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent productreceipt=new Intent(MainActivity.this, ProductReceive.class);
+                startActivity(productreceipt);
+            }
+        });
+
+        productdispatchbutton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent productdispatch=new Intent(MainActivity.this, ProductDispatch.class);
+                startActivity(productdispatch);
+            }
+        });
     }
 }
