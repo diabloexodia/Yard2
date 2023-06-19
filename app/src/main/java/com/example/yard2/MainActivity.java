4bec3fe557d3a4f170020d5e4577937d982fd506
Pranith12250 <127399614+Pranith12250@users.noreply.github.com>
2023-06-19 10:20:39 +0530

    activity main created

--- app/src/main/java/com/example/yard2/MainActivity.java
+++ app/src/main/java/com/example/yard2/MainActivity.java
@@ -5,18 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println(1);
-        System.out.println();
-
-
-        Toast.makeText(this, "New test change", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "PRANITH MADE THIS CHANGE BRO", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "Final", Toast.LENGTH_SHORT).show();
     }
 }
