2e826089cae17d69eae31e79a35bd19e4c6abec4
Diablo <diabloexodia@gmail.com>
2023-06-19 14:42:09 +0530

    added product received page

--- app/src/main/java/com/example/yard2/MainActivity.java
+++ app/src/main/java/com/example/yard2/MainActivity.java
@@ -2,20 +2,31 @@ package com.example.yard2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button productReceive;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println(1);
-        System.out.println();
+
+        productReceive=findViewById(R.id.productreceivingbutton);
+
+        productReceive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),ProductReceive.class);
+                startActivity(intent);
+            }
+        });
 
 
-        Toast.makeText(this, "New test change", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "PRANITH MADE THIS CHANGE BRO", Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/com/example/yard2/ProductReceive.java
+++ app/src/main/java/com/example/yard2/ProductReceive.java
@@ -2,14 +2,62 @@ package com.example.yard2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ProductReceive extends AppCompatActivity {
 
+    static String product_id,product_grade,product_description;
+    static int bin_number;
+    Button materialScanButton,binScanbutton,generateReceipt;
+    EditText quantityEdittext;
+
+    @SuppressLint({"MissingInflatedId", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_receive);
-        // this is the new commit without somehting
+
+        materialScanButton=findViewById(R.id.materialScan);
+        binScanbutton=findViewById(R.id.binScan);
+        generateReceipt=findViewById(R.id.generateReceipt);
+        quantityEdittext= findViewById(R.id.qtyEditText);
+
+
+
+        materialScanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //assign value for  product_id,product_grade,product_description
+            }
+        });
+
+
+        binScanbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //assign value for bin_number
+            }
+        });
+        generateReceipt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            //    Toast.makeText(ProductReceive.this,quantityEdittext.getText().toString() , Toast.LENGTH_SHORT).show();
+
+                if(quantityEdittext==null)
+                    Toast.makeText(ProductReceive.this, "Input Quantity", Toast.LENGTH_SHORT).show();
+                else
+                {
+                    //Databse input code
+                }
+            }
+        });
+
+
     }
 }
