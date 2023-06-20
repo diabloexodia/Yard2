b961d4a0c076cfc202860af6960f03abe1205377
Pranith12250 <127399614+Pranith12250@users.noreply.github.com>
2023-06-20 15:01:05 +0530

    newest

--- app/src/main/java/com/example/yard2/ProductReceive.java
+++ app/src/main/java/com/example/yard2/ProductReceive.java
@@ -27,7 +27,7 @@ public class ProductReceive extends AppCompatActivity
     @SuppressLint({"MissingInflatedId", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState)
-    {
+    { //
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_receive);
 
