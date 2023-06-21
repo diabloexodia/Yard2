eb3f21a510628f578cdf7a7a2bbaa53409171c8c
Pranith12250 <127399614+Pranith12250@users.noreply.github.com>
2023-06-21 13:54:57 +0530

    product dispatch part 1

--- app/src/main/java/com/example/yard2/ProductDispatch.java
+++ app/src/main/java/com/example/yard2/ProductDispatch.java
@@ -2,14 +2,24 @@ package com.example.yard2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ProductDispatch extends AppCompatActivity
 {
+    Button binScanner, materialScanner,generate;
+    EditText sales_order_id;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_dispatch);
+        binScanner=findViewById(R.id.binScan2);
+        materialScanner=findViewById(R.id.materialScan2);
+        sales_order_id=findViewById(R.id.soiEditText);
+        generate=findViewById(R.id.generateReceipt2);
     }
 }
