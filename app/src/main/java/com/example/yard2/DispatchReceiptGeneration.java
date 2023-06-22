f6b7e080d5d995135d7de0781897afd9cac65bd5
Diablo <diabloexodia@gmail.com>
2023-06-22 15:05:37 +0530

    Bug fixes v1.23.4

--- app/src/main/java/com/example/yard2/DispatchReceiptGeneration.java
+++ app/src/main/java/com/example/yard2/DispatchReceiptGeneration.java
@@ -1,5 +1,7 @@
 package com.example.yard2;
 
+import static com.example.yard2.ReceiptGeneration.generateRandomString;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -15,62 +17,84 @@ import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class DispatchReceiptGeneration extends AppCompatActivity
-{
-    TextView dispatchidtext,  datetext, productidtext, salesorderidtext, quantitytext;
+public class DispatchReceiptGeneration extends AppCompatActivity {
+    TextView dispatchidtext, datetext, productidtext, salesorderidtext, quantitytext;
     EditText remarkset;
     Spinner transporttypespin;
     Button submitbutton;
-    private String transporttypestring, remarksstring;
+    private String transporttypestring = " ";
+
     @SuppressLint("MissingInflatedId")
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dispatch_receipt_generation);
         getSupportActionBar().setTitle("Generate Dispatch Receipt");
 
-        dispatchidtext=findViewById(R.id.dispatchIdEditText);
-        datetext=findViewById(R.id.dateEdittext2);
-        productidtext=findViewById(R.id.productIdEditText2);
-        salesorderidtext=findViewById(R.id.salesOrderEditText);
-        quantitytext=findViewById(R.id.QuantityEdittext2);
-        remarkset=findViewById(R.id.RemarksEditText2);
-        transporttypespin=findViewById(R.id.transportTypeSpinner2);
-        submitbutton=findViewById(R.id.ReceiptGenerationSubmitButton2);
+        dispatchidtext = findViewById(R.id.dispatchIdEditText);
+        datetext = findViewById(R.id.dateEdittext2);
+        productidtext = findViewById(R.id.productIdEditText2);
+        salesorderidtext = findViewById(R.id.salesOrderEditText);
+        quantitytext = findViewById(R.id.QuantityEdittext2);
+        remarkset = findViewById(R.id.RemarksEditText2);
+        transporttypespin = findViewById(R.id.transportTypeSpinner2);
+        submitbutton = findViewById(R.id.ReceiptGenerationSubmitButton2);
 
-        Intent currentIntent=getIntent();
+        Intent currentIntent = getIntent();
 
         Date currentDate = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         datetext.setText(dateFormat.format(currentDate));
 
-        transporttypespin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
-        {
+        transporttypespin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
-            {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 transporttypestring = parent.getItemAtPosition(position).toString();
             }
+
             @Override
-            public void onNothingSelected(AdapterView<?> parent)
-            {
+            public void onNothingSelected(AdapterView<?> parent) {
                 transporttypestring = parent.getItemAtPosition(0).toString();
             }
         });
 
         productidtext.setText(currentIntent.getStringExtra("productid"));
         salesorderidtext.setText(currentIntent.getStringExtra("salesorderid"));
-        quantitytext.setText(String.valueOf(currentIntent.getDoubleExtra("Quantity",-1)));
+        quantitytext.setText(String.valueOf(currentIntent.getDoubleExtra("Quantity", -1)));
+        // -----------Date-----------------------------------------------------
+        currentDate = new Date();
+        dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        datetext.setText(dateFormat.format(currentDate));
+        // -----------Date------------------------------------------------------
 
-        remarksstring=remarkset.getText().toString();
-
-        submitbutton.setOnClickListener(new View.OnClickListener()
-        {
+        // ------------Transport Type---------------------------------------------------------------
+        transporttypespin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v)
-            {
-                Intent successdispatchpage=new Intent(DispatchReceiptGeneration.this, SuccessPageDispatch.class);
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                transporttypestring = parent.getItemAtPosition(position).toString();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                transporttypestring = parent.getItemAtPosition(0).toString();
+            }
+        });
+        // ------------Transport Type---------------------------------------------------------------
+        dispatchidtext.setText(generateRandomString(6));
+
+
+        submitbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent successdispatchpage = new Intent(DispatchReceiptGeneration.this, SuccessPageDispatch.class);
+                successdispatchpage.putExtra("Product ID", productidtext.getText().toString());
+                successdispatchpage.putExtra("Quantity", quantitytext.getText().toString());
+                successdispatchpage.putExtra("Sales ID", salesorderidtext.getText().toString());
+                successdispatchpage.putExtra("Date", datetext.getText().toString());
+                successdispatchpage.putExtra("Remarks", remarkset.getText().toString());
+                successdispatchpage.putExtra("Transport", transporttypestring);
                 startActivity(successdispatchpage);
             }
         });
