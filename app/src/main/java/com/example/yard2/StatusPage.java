09db88e975caf1ea640652271f532c877d2b13e0
Pranith12250 <127399614+Pranith12250@users.noreply.github.com>
2023-06-23 10:17:05 +0530

    new activities

---
+++ app/src/main/java/com/example/yard2/StatusPage.java
@@ -0,0 +1,43 @@
+package com.example.yard2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StatusPage extends AppCompatActivity
+{
+    Button productstatus, binstatus;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status_page);
+        productstatus=findViewById(R.id.productstatus);
+        binstatus=findViewById(R.id.binstatus);
+
+        productstatus.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent productstatuspage=new Intent(StatusPage.this,ProductStatus.class);
+                startActivity(productstatuspage);
+            }
+        });
+
+        binstatus.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent binstatuspage=new Intent(StatusPage.this, BinStatus.class);
+                startActivity(binstatuspage);
+            }
+        });
+    }
+}
